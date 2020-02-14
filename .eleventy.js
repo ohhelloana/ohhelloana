83802b1f2fb77704454729da511564fed60988f9
Ana R <ana.rdrg@gmail.com>
2020-02-14 16:05:24 +0000

    Update eleventy configuration to call the correct layout files; tell posts what layout to use;

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addLayoutAlias('bookmarks', 'layouts/bookmarks.html');
-  eleventyConfig.addLayoutAlias('bookmarktagpage', 'layouts/bookmarktagpage.html');
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
-  eleventyConfig.addLayoutAlias('home', 'layouts/home.html');
-  eleventyConfig.addLayoutAlias('micropubpost', 'layouts/micropubpost.html');
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
-  eleventyConfig.addLayoutAlias('rsvp', 'layouts/rsvp.html');
+  eleventyConfig.addLayoutAlias('bookmarks', 'layouts/bookmarks.njk');
+  eleventyConfig.addLayoutAlias('bookmarktagpage', 'layouts/bookmarktagpage.njk');
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk');
+  eleventyConfig.addLayoutAlias('home', 'layouts/home.njk');
+  eleventyConfig.addLayoutAlias('micropubpost', 'layouts/micropubpost.njk');
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+  eleventyConfig.addLayoutAlias('rsvp', 'layouts/rsvp.njk');
 
-  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('src/assets');
 
   return {
     dir: {
       input: "src",
-      output: "site",
+      output: "site"
     },
     passthroughFileCopy: true
   };
